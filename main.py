633ee9fc4672a25d8e9db66b3a775d30c201fdf3
justinjest <jest.cayaban@gmail.com>
2024-09-20 10:05:16 +0900

    Updated base case to v3

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ["https://www.googleapis.com/auth/drive.activity.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+
 
 def main():
     creds = None
@@ -19,7 +20,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "api_keys/credentils.json", SCOPES
+                "api_keys/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
     # Save credentials
@@ -30,80 +31,26 @@ def main():
 
     # Calling api here
     try:
-        results = service.activity().query(body={"pageSize": 10}).execute()
-        activities = results.get("activities", [])
+        service = build("drive", "v3", credentials=creds)
 
-        if not activities:
-            print("No activity.")
-        else:
-            print("Recent activity:")
-            for activity in activities:
-                time = getTimeInfo(activity)
-                action = getActionInfo(activity["primaryActionDetail"])
-                actors = map(getActorInfo, activity["actors"])
-                targets = map(getTargetInfo, activity["targets"])
-                actors_str, targets_str = "", ""
-                actor_name = actors_str.join(actors)
-                target_name = targets_str.join(targets)
+        # Call the Drive v3 API
+        results = (
+            service.files()
+            .list(pageSize=10, fields="nextPageToken, files(id, name)")
+            .execute()
+    )
+        items = results.get("files", [])
 
-                # Print the action occurred on drive with actor, target item and timestamp
-                print(f"{time}: {action}, {actor_name}, {target_name}")
-
+        if not items:
+            print("No files found.")
+            return
+        print("Files:")
+        for item in items:
+            print(f"{item['name']} ({item['id']})")
     except HttpError as error:
-        # TODO(developer) - Handleerrors from drive activity API.
+        # TODO(developer) - Handle errors from drive API.
         print(f"An error occurred: {error}")
 
-
-    # Returns the name of a set property in an object, or else "unknown".
-def getOneOf(obj):
-    for key in obj:
-        return key
-    return "unknown"
-
-
-# Returns a time associated with an activity.
-def getTimeInfo(activity):
-    if "timestamp" in activity:
-        return activity["timestamp"]
-    if "timeRange" in activity:
-        return activity["timeRange"]["endTime"]
-    return "unknown"
-
-
-# Returns the type of action.
-def getActionInfo(actionDetail):
-    return getOneOf(actionDetail)
-
-
-# Returns user information, or the type of user if not a known user.
-def getUserInfo(user):
-    if "knownUser" in user:
-        knownUser = user["knownUser"]
-        isMe = knownUser.get("isCurrentUser", False)
-        return "people/me" if isMe else knownUser["personName"]
-    return getOneOf(user)
-
-
-# Returns actor information, or the type of actor if not a user.
-def getActorInfo(actor):
-    if "user" in actor:
-        return getUserInfo(actor["user"])
-    return getOneOf(actor)
-
-
-# Returns the type of a target and an associated title.
-def getTargetInfo(target):
-    if "driveItem" in target:
-        title = target["driveItem"].get("title", "unknown")
-        return f'driveItem:"{title}"'
-    if "drive" in target:
-        title = target["drive"].get("title", "unknown")
-        return f'drive:"{title}"'
-    if "fileComment" in target:
-        parent = target["fileComment"].get("parent", {})
-        title = parent.get("title", "unknown")
-        return f'fileComment:"{title}"'
-        return f"{getOneOf(target)}:unknown"
 
 if __name__ == "__main__":
     print ("Hello world")
