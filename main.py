10a4a3efaee1c9509a8a44d39a9e2f35eba1818e
justinjest <jest.cayaban@gmail.com>
2024-09-20 10:54:20 +0900

    Added basic checker to determine if shared links are outstanding

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
 def main():
     creds = None
-
+    print ("Conncecting to google drive")
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
@@ -26,13 +26,15 @@ def main():
     # Save credentials
     with open("token.json", "w") as token:
         token.write(creds.to_json())
-
+    print ("Logged in, accessing API")
     # Calling api here
     try:
         files = []
+        risk_detection = []
         service = build("drive", "v3", credentials=creds)
 
         # Call the Drive v3 API
+        print ("Starting to scan files")
         results = (
             service.files()
             .list(pageSize=10, fields="nextPageToken, files(id, name)")
@@ -45,20 +47,41 @@ def main():
             return
         print("Files:")
         for item in items:
-            files.append(item['id'])
+            files.append((item['name'], item['id']))
             print(f"{item['name']} ({item['id']})")
-
+        print ("Found all items")
+        print ("Scanning for shared links")
         for file in files:
-            print (results.get(file, []))
-
+            file_roles = get_file_roles(file[1], creds)
+            for item in file_roles:
+                if {item['id']} == {'anyoneWithLink'}:
+                    risk_detection.append(file[0])
+        print ("Following files have an outstanding vulnerability")
+        print (risk_detection)
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
         print(f"An error occurred: {error}")
+# Authenticate and create the Drive API client
+
 
 # File -> Permision JSON
-def getPermissions(file):
-    result = file.list("permissions(id)")
-    return result
+def get_file_roles(file_id, creds):
+    try:
+        service = build('drive', 'v3', credentials = creds)
+        permissions = service.permissions().list(fileId=file_id).execute()
+        
+        roles = []
+        for permission in permissions.get('permissions', []):
+            roles.append({
+                'id': permission.get('id'),
+                'role': permission.get('role'),
+                'emailAddress': permission.get('emailAddress', 'N/A')
+            })
+        # Returns user ID's of all accounts that have access
+
+        return roles
+    except:
+        return []
 
 if __name__ == "__main__":
     print ("Hello world")
